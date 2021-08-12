14a36ec65b190fb7a7cfec4596eaf621bfc06351
Molekulah <bruno@economicros.com.br>
2021-08-12 18:10:15 -0300

    Now the matches are true

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,23 +3,81 @@ const cors = require("cors");
 const riotApi = require("./services/riotApi");
 const app = express();
 
+const getMatchiesData = async (matchIds) => {
+  const matchiesData = await Promise.all(
+    matchIds.map(async (matchId) => {
+      const { data } = await riotApi.get(`/lol/match/v4/matches/${matchId}`);
+
+      return data;
+    })
+  );
+
+  const formattedData = matchiesData.map((match) => {
+    const participants = match.participantIdentities
+      .filter((pi) => {
+        if (
+          pi.player.summonerName === "Ca9" ||
+          pi.player.summonerName === "luahp" ||
+          pi.player.summonerName === "fast blades"
+        ) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+      .map((p) => {
+        return {
+          id: p.participantId,
+          name: p.player.summonerName,
+        };
+      });
+
+    const participantsIds = participants.map((p) => p.id);
+    const participantInfos = match.participants.filter((math_participant) => {
+      return participantsIds.includes(math_participant.participantId);
+    });
+
+    const kdas = participantInfos.map((pf) => {
+      const { name } = participants.find((p) => p.id === pf.participantId);
+
+      return {
+        name: name,
+        lane: pf.timeline.lane,
+        id: pf.participantId,
+        kills: pf.stats.kills,
+        deaths: pf.stats.deaths,
+        assists: pf.stats.assists,
+      };
+    });
+
+    return {
+      matchId: match.gameId,
+      kdas: kdas,
+    };
+  });
+
+  return formattedData;
+};
+
 app.use(cors());
 
 app.get("/partidas", async (req, res) => {
-  const { data } = await riotApi.get(
-    "/lol/match/v4/matchlists/by-account/XZ4DjuUYDzApUwzOtc5EWekItbLw86DeHXcSkxS-f2xx3YM",
-    { params: { beginIndex: 0, endIndex: 10 } }
-  );
+  try {
+    const { data } = await riotApi.get(
+      "/lol/match/v4/matchlists/by-account/C2IofxYQuHjkF9Aw_fJ7WSelXAnech1CoDKJlodngAhsk0I",
+      { params: { beginIndex: 0, endIndex: 10 } }
+    );
 
-  console.log(data.matches);
-  const partidas = data.matches;
-  
-  const partidasIds = partidas.map((partida) => partida.gameId);
-  const { data: match } = await riotApi.get("/lol/match/v4/matches/2330840761");
-  
-const
+    const matches = data.matches;
 
-  return res.send({ message: partidasIds });
+    const matchIds = matches.map((match) => match.gameId);
+
+    const matchiesData = await getMatchiesData(matchIds);
+
+    return res.send({ matchies: matchiesData });
+  } catch (err) {
+    return res.send({ message: err.message });
+  }
 });
 
 app.listen(8080, () => {
--- backend/src/services/riotApi.js
+++ backend/src/services/riotApi.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const riotApi = axios.create({
   baseURL: "https://br1.api.riotgames.com",
   params: {
-    api_key: "RGAPI-55f751de-2f91-43cf-89a9-5f96146f6026",
+    api_key: "RGAPI-1209ccfa-19b6-4d70-92e5-1da6b0e96bd1",
   },
 });
 
