8e6f96fb5c9687a9547b233c01d988053557437d
Molekulah <bruno@economicros.com.br>
2021-08-17 17:02:47 -0300

    Changed strings of feelings from text to emoji

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { api } from './services/api';
-import './App.css';
+import { api } from "./services/api";
+import "./App.css";
 
 const App = () => {
   const [partidas, setPartidas] = useState([]);
 
   useEffect(() => {
     const fetchMatchies = async () => {
-      const { data } = await api.get('/partidas');
+      const { data } = await api.get("/partidas");
       console.log(data.matchies);
       setPartidas(data.matchies);
     };
@@ -19,7 +19,7 @@ const App = () => {
   return (
     <div
       className=" cover-container  text-center text-light w-100 h-100 mx-auto flex-column bg-dark"
-      style={{ height: '100vh' }}
+      style={{ height: "100vh" }}
     >
       <div className="px-4 py-5 text-center">
         <p className="h1 display-2 p-2 fw-bold">League of Fellings</p>
@@ -32,21 +32,21 @@ const App = () => {
         </div>
       </div>
 
-      {partidas?.map(partida => (
+      {partidas?.map((partida) => (
         <div className="container " key={partida.matchId}>
           {
             <div className="container">
               <div className="row ">
                 <div
                   className={` ${
-                    partida.totalFeelings === 'sad' ? 'bg-danger' : 'bg-primary'
+                    partida.totalFeelings === "sad" ? "bg-danger" : "bg-primary"
                   } align-items-center justify-content-center d-flex `}
                 >
                   <p className="h2" style={{ margin: 20 }}>
-                    {partida.totalFeelings === 'so sad' && 'TRISTEZA'}
-                    {partida.totalFeelings === 'sad' && 'TRISTEZA'}
-                    {partida.totalFeelings === 'so happy' && 'FELICIDADE'}
-                    {partida.totalFeelings === 'happy' && 'FELICIDADE'}
+                    {partida.totalFeelings === "🙁" && "TRISTEZA"}
+                    {partida.totalFeelings === "😭" && "TRISTEZA"}
+                    {partida.totalFeelings === "😀" && "FELICIDADE"}
+                    {partida.totalFeelings === "😁" && "FELICIDADE"}
                   </p>
                   <br></br>
                   <div style={{ fontSize: 50 }}>{partida.totalFeelings}</div>
@@ -54,9 +54,9 @@ const App = () => {
               </div>
               <table
                 className={` ${
-                  partida.totalFeelings === 'sad'
-                    ? 'table-danger'
-                    : 'table-primary'
+                  partida.totalFeelings === "sad"
+                    ? "table-danger"
+                    : "table-primary"
                 } table  `}
               >
                 <thead>
@@ -68,7 +68,7 @@ const App = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {partida?.kdas?.map(kda => (
+                  {partida?.kdas?.map((kda) => (
                     <tr key={kda.name}>
                       <th scope="row">{kda.name}</th>
                       <td>{kda.lane}</td>
